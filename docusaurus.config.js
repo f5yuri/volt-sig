680c2199aa4802e6dcd46690693f63e59c554e11
Yuri Quispe <g3yuri@gmail>
2023-05-22 01:10:03 -0500

    fonos

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/f5yuri/volt-sig/tree/main/docs/',
+            'https://github.com/f5yuri/volt-sig/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/f5yuri/volt-sig/tree/main/blog',
+            'https://github.com/f5yuri/volt-sig/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
